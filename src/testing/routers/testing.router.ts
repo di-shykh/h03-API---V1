501906ee7695db7e478e7bade7bbdf5c6883a219
di-shykh <di49@mail.ru>
2025-11-30 23:15:24 +0300

    feat: change clearing DB

--- src/testing/routers/testing.router.ts
+++ src/testing/routers/testing.router.ts
@@ -1,12 +1,14 @@
 import {Router, Request, Response} from 'express';
-import {db} from '../../db/in-memory.db';
 import {HttpStatus} from "../../core/types/http-statuses";
+import {blogCollection, postCollection} from "../../db/mongo.bd";
 
 export const testingRouter: Router = Router({});
 
-testingRouter.delete('/all-data', (req: Request, res: Response): void => {
+testingRouter.delete('/all-data',async (req: Request, res: Response): Promise<void> => {
     console.log('✅ Testing endpoint called'); // для диагностики
-    db.blogs = [];
-    db.posts = [];
+    await Promise.all([
+        blogCollection.deleteMany(),
+        postCollection.deleteMany(),
+    ])
     res.sendStatus(HttpStatus.NoContent);
 })
